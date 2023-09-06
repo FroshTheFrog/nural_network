799af476164f7f5bffc6d937a1724071cc9406d3
FroshTheFrog <28665112+FroshTheFrog@users.noreply.github.com>
2023-09-05 20:13:06 -0500

    added network dimension verification

--- nural_network_from_stratch/src/types/network.rs
+++ nural_network_from_stratch/src/types/network.rs
@@ -12,7 +12,10 @@ impl<const SIZE: usize> Perceptron<{ SIZE }> {
     }
 }
 
-trait Layer {}
+trait Layer {
+    fn get_input_size(&self) -> usize;
+    fn get_output_size(&self) -> usize;
+}
 
 struct NeuralLayer<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> {
     perceptions: [Perceptron<INPUT_SIZE>; OUTPUT_SIZE],
@@ -31,14 +34,41 @@ impl<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> NeuralLayer<INPUT_SIZE, 
 impl<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> Layer
     for NeuralLayer<INPUT_SIZE, OUTPUT_SIZE>
 {
+    fn get_input_size(&self) -> usize {
+        INPUT_SIZE
+    }
+
+    fn get_output_size(&self) -> usize {
+        OUTPUT_SIZE
+    }
 }
 
+pub type LayerList<const NUMBER_OF_LAYERS: usize> = [Box<dyn Layer>; NUMBER_OF_LAYERS];
+
 pub struct Network<const NUMBER_OF_LAYERS: usize> {
-    layers: [Box<dyn Layer>; NUMBER_OF_LAYERS],
+    layers: LayerList<NUMBER_OF_LAYERS>,
 }
 
 impl<const NUMBER_OF_LAYERS: usize> Network<NUMBER_OF_LAYERS> {
-    pub fn new(layers: [Box<dyn Layer>; NUMBER_OF_LAYERS]) -> Self {
-        Network { layers: layers }
+    pub fn new(layers: LayerList<NUMBER_OF_LAYERS>) -> Option<Self> {
+        if Self::verify_layer_dimensions(&layers) {
+            return Some(Network { layers });
+        }
+        None
+    }
+
+    fn verify_layer_dimensions(layers: &LayerList<NUMBER_OF_LAYERS>) -> bool {
+        let mut previous_layer_output_Size = layers[0].get_output_size();
+
+        for layer in layers.iter().skip(1) {
+            let current_layer_input_size = layer.get_input_size();
+
+            if current_layer_input_size != previous_layer_output_Size {
+                return false;
+            }
+
+            previous_layer_output_Size = layer.get_output_size();
+        }
+        true
     }
 }
