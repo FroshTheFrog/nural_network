bb167a1f391517de5d30073531cecce0554e9fdc
FroshTheFrog <28665112+FroshTheFrog@users.noreply.github.com>
2023-09-08 21:24:19 -0500

    added feed forward and switched to vectors

--- nural_network_from_stratch/src/types/network.rs
+++ nural_network_from_stratch/src/types/network.rs
@@ -1,74 +1,95 @@
-struct Perceptron<const SIZE: usize> {
-    weights: [f64; SIZE],
+type ActivationFunction = fn(f64) -> f64;
+type WeightInitializer = fn() -> f64;
+
+struct Perceptron {
+    weights: Vec<f64>,
     bias: f64,
 }
 
-impl<const SIZE: usize> Perceptron<{ SIZE }> {
-    fn new(weight_initializer: fn() -> f64) -> Self {
-        Perceptron {
-            weights: [(); SIZE].map(|_| weight_initializer()),
-            bias: weight_initializer(),
-        }
+impl Perceptron {
+    fn new(weight_initializer: WeightInitializer, size: usize) -> Self {
+        let weights = (0..size).map(|_| weight_initializer()).collect();
+        let bias = weight_initializer();
+
+        Self { weights, bias }
+    }
+
+    fn feed_forward(&self, inputs: &[f64]) -> f64 {
+        self.weights
+            .iter()
+            .zip(inputs)
+            .fold(self.bias, |acc, (w, i)| acc + w * i)
     }
 }
 
 trait Layer {
     fn get_input_size(&self) -> usize;
     fn get_output_size(&self) -> usize;
+    fn feed_forward(&self, inputs: &[f64]) -> Vec<f64>;
 }
 
-struct NeuralLayer<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> {
-    perceptions: [Perceptron<INPUT_SIZE>; OUTPUT_SIZE],
-    activation_function: fn(f64) -> f64,
+struct NeuralLayer {
+    perceptrons: Vec<Perceptron>,
+    activation_function: ActivationFunction,
 }
 
-impl<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> NeuralLayer<INPUT_SIZE, OUTPUT_SIZE> {
-    fn new(activation_function: fn(f64) -> f64, weight_initializer: fn() -> f64) -> Self {
-        NeuralLayer {
-            perceptions: [(); OUTPUT_SIZE].map(|_| Perceptron::new(weight_initializer)),
-            activation_function: activation_function,
+impl NeuralLayer {
+    fn new(
+        activation_function: ActivationFunction,
+        weight_initializer: WeightInitializer,
+        input_size: usize,
+        output_size: usize,
+    ) -> Self {
+        let perceptrons = (0..output_size)
+            .map(|_| Perceptron::new(weight_initializer, input_size))
+            .collect();
+
+        Self {
+            perceptrons,
+            activation_function,
         }
     }
 }
 
-impl<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> Layer
-    for NeuralLayer<INPUT_SIZE, OUTPUT_SIZE>
-{
+impl Layer for NeuralLayer {
     fn get_input_size(&self) -> usize {
-        INPUT_SIZE
+        self.perceptrons[0].weights.len()
     }
 
     fn get_output_size(&self) -> usize {
-        OUTPUT_SIZE
+        self.perceptrons.len()
+    }
+
+    fn feed_forward(&self, inputs: &[f64]) -> Vec<f64> {
+        self.perceptrons
+            .iter()
+            .map(|p| (self.activation_function)(p.feed_forward(inputs)))
+            .collect()
     }
 }
 
-pub type LayerList<const NUMBER_OF_LAYERS: usize> = [Box<dyn Layer>; NUMBER_OF_LAYERS];
-
-pub struct Network<const NUMBER_OF_LAYERS: usize> {
-    layers: LayerList<NUMBER_OF_LAYERS>,
+pub struct Network {
+    layers: Vec<Box<dyn Layer>>,
 }
 
-impl<const NUMBER_OF_LAYERS: usize> Network<NUMBER_OF_LAYERS> {
-    pub fn new(layers: LayerList<NUMBER_OF_LAYERS>) -> Option<Self> {
-        if Self::verify_layer_dimensions(&layers) {
-            return Some(Network { layers });
-        }
-        None
+impl Network {
+    pub fn new(layers: Vec<Box<dyn Layer>>) -> Self {
+        assert!(
+            Self::verify_layer_dimensions(&layers),
+            "Invalid layer dimensions"
+        );
+        Self { layers }
     }
 
-    fn verify_layer_dimensions(layers: &LayerList<NUMBER_OF_LAYERS>) -> bool {
-        let mut previous_layer_output_Size = layers[0].get_output_size();
+    fn verify_layer_dimensions(layers: &[Box<dyn Layer>]) -> bool {
+        layers
+            .windows(2)
+            .all(|w| w[0].get_output_size() == w[1].get_input_size())
+    }
 
-        for layer in layers.iter().skip(1) {
-            let current_layer_input_size = layer.get_input_size();
-
-            if current_layer_input_size != previous_layer_output_Size {
-                return false;
-            }
-
-            previous_layer_output_Size = layer.get_output_size();
-        }
-        true
+    pub fn feed_forward(&self, input: Vec<f64>) -> Vec<f64> {
+        self.layers
+            .iter()
+            .fold(input, |acc, layer| layer.feed_forward(&acc))
     }
 }
