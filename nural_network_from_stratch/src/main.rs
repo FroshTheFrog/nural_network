f9a24288fa1cb73d66ba407303cc41a94404dc78
FroshTheFrog <28665112+FroshTheFrog@users.noreply.github.com>
2023-09-10 02:31:05 -0500

    kind of works now

--- nural_network_from_stratch/src/main.rs
+++ nural_network_from_stratch/src/main.rs
@@ -1,25 +1,47 @@
 use network::functions::create_random_weight_initializer;
-use network::functions::RELU;
+use network::functions::Relu;
 use network::layers::Layer;
 use network::layers::NeuralLayer;
 use network::network::Network;
 
-use crate::network::functions::SIGMOID;
+use crate::network::functions::MeanSquaredError;
 
 mod network;
 
 fn main() {
-    let mut layer0 = NeuralLayer::new(&RELU, create_random_weight_initializer::<-1, 1>(), 5, 5);
-    let mut layer1 = NeuralLayer::new(&SIGMOID, create_random_weight_initializer::<-1, 1>(), 5, 5);
-    let mut layer2 = NeuralLayer::new(&RELU, create_random_weight_initializer::<-1, 1>(), 5, 3);
+    let mut layer0 = NeuralLayer::new(&Relu, create_random_weight_initializer::<-1, 1>(), 3, 3);
+    let mut layer1 = NeuralLayer::new(&Relu, create_random_weight_initializer::<-1, 1>(), 3, 3);
+    let mut layer2 = NeuralLayer::new(&Relu, create_random_weight_initializer::<-1, 1>(), 3, 2);
+    let mut layer3 = NeuralLayer::new(&Relu, create_random_weight_initializer::<-1, 1>(), 2, 2);
+    let mut layer4 = NeuralLayer::new(&Relu, create_random_weight_initializer::<-1, 1>(), 2, 2);
 
-    let layers: Vec<&mut dyn Layer> = vec![&mut layer0, &mut layer1, &mut layer2];
+    let layers: Vec<&mut dyn Layer> = vec![
+        &mut layer0,
+        &mut layer1,
+        &mut layer2,
+        &mut layer3,
+        &mut layer4,
+    ];
 
-    let test_network = Network::new(layers);
+    let mut test_network = Network::new(layers);
 
-    let input = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+    let input = vec![[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]];
+    let expected = vec![[1.0, 0.0], [0.0, 1.0], [0.0, 0.0]];
 
-    let (output, derivative) = test_network.feed_forward(input, false);
+    let number_of_epocs = 100;
 
-    println!("{:?}", output);
+    let learning_rate = 0.01;
+
+    for _ in 0..number_of_epocs {
+        for (input, expected) in input.iter().zip(expected.iter()) {
+            let cost = test_network.train(
+                input.to_vec(),
+                expected.to_vec(),
+                &MeanSquaredError,
+                learning_rate,
+            );
+
+            println!("Cost: {}", cost);
+        }
+    }
 }
--- nural_network_from_stratch/src/network/functions.rs
+++ nural_network_from_stratch/src/network/functions.rs
@@ -21,17 +21,48 @@ pub fn create_random_weight_initializer<const LOWER_BOUND: i64, const UPPER_BOUN
     }
 }
 
-pub const RELU: fn(f64) -> f64 = |x| if x > 0.0 { x } else { 0.0 };
+pub struct Relu;
+
+impl ActivationFunction for Relu {
+    fn activate(&self, x: f64) -> f64 {
+        if x > 0.0 {
+            x
+        } else {
+            0.0
+        }
+    }
+
+    fn derivative(&self, x: f64) -> f64 {
+        if x > 0.0 {
+            1.0
+        } else {
+            0.0
+        }
+    }
+}
+
 pub const SIGMOID: fn(f64) -> f64 = |x| 1.0 / (1.0 + (-x).exp());
 
-pub const MEAN_SQUARED_ERROR: fn(&[f64], &[f64]) -> f64 = |output, expected| {
-    output
-        .iter()
-        .zip(expected)
-        .map(|(o, e)| (o - e).powi(2))
-        .sum::<f64>()
-        / output.len() as f64
-};
+pub struct MeanSquaredError;
+
+impl CostFunction for MeanSquaredError {
+    fn cost(&self, output: &[f64], expected: &[f64]) -> f64 {
+        output
+            .iter()
+            .zip(expected)
+            .map(|(o, e)| (o - e).powi(2))
+            .sum::<f64>()
+            / output.len() as f64
+    }
+
+    fn derivative(&self, output: &[f64], expected: &[f64]) -> Vec<f64> {
+        output
+            .iter()
+            .zip(expected)
+            .map(|(o, e)| 2.0 * (o - e))
+            .collect()
+    }
+}
 
 impl<F> ActivationFunction for F
 where
